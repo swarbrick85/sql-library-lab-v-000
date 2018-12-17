141d1628f981b89933ceb5544d8459db6b861ac3
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-17 22:15:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name, series.subgenre_"
+  "SELECT author.name, subgenre.name "
 end
 
 def select_series_title_with_most_human_characters
