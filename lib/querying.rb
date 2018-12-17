468d7f48dcd301ca598b909f84e7d8a9177b250b
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-17 22:36:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT("
+  "SELECT characters.name, COUNT(characters.name)
+   FROM (books JOIN series ON series.id = books.series_id) 
+   JOIN characters
+   ON series.id = characters.series_id
+   "
 end
