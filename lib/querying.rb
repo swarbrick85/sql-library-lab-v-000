f46dc14732a627d5cd46357d211f2aa4913b55a8
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-17 23:04:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -34,8 +34,9 @@ def select_series_title_with_most_human_characters
    FROM series 
    JOIN characters 
    ON series.id = characters.series_id
-   GROUP BY series.title, characters.species
-   ORDER BY COUNT(species)
+   WHERE characters.species = 'human'
+   GROUP BY series.title
+   ORDER BY COUNT(species) DESC LIMIT 1 
    "
 end
 
