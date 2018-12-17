a2718501c87b54917870b9949ca2d989e066230e
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-12-17 22:15:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,29 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year 
+   FROM books 
+   JOIN series 
+   ON books.series_id = series.id 
+   WHERE series.id = 1 
+   ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto 
+   FROM characters
+   WHERE length(motto) = (SELECT MAX(length(motto)) FROM characters)"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+   FROM characters 
+   GROUP BY species 
+   ORDER BY COUNT(species) DESC LIMIT 1
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT author.name, series.subgenre_"
 end
 
 def select_series_title_with_most_human_characters
